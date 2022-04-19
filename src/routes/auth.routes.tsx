53a2de02662fd96dd8942b47d35f2f364b746736
Donizete Waterkemper <dwater2@hotmail.com>
2022-04-18 22:14:58 -0300

    first commit

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { SignIn } from '@screens/SignIn';
+import { Register } from '@screens/Register';
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AuthRoutes() {
+
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="signIn" component={SignIn} />
+      <Screen name="register" component={Register} />
+    </Navigator>
+  );
+}
